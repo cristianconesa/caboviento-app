05735456ab7035ad5236e5c6dcb096a0003fc465
Cristian Conesa <cristiancitoconesa@gmail.com>
2025-09-30 02:23:01 -0300

    login google deshabilitado temporalmente, edicion de versiones react y react native, dependencias faltantes

--- App.tsx
+++ App.tsx
@@ -2,22 +2,18 @@
 
 
 import React, { useState } from 'react';
-import "nativewind/types";
 import AppNavigator from './navigation';
 import { ThemeProvider } from './components/ThemeProvider';
 import LoginScreen from './screens/LoginScreen';
 
 
 export default function App() {
-  const [user, setUser] = useState<any>(null);
+  // Simular usuario logueado temporalmente para navegar la app
+  const [user] = useState<any>({ id: 1, name: 'Usuario Demo' });
 
   return (
     <ThemeProvider>
-      {user ? (
-        <AppNavigator />
-      ) : (
-        <LoginScreen onLogin={setUser} />
-      )}
+      <AppNavigator />
     </ThemeProvider>
   );
 }
